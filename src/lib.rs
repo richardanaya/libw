f4ab7ae8b10338e264aace8d1255c684e32077b6
Richard <Richard>
2020-03-03 14:07:22 -0800

    adding randomness

--- src/lib.rs
+++ src/lib.rs
@@ -30,22 +30,22 @@ pub fn command_line_arguments() -> Vec<String> {
     let mut cmd_args: Vec<String> = vec![];
     unsafe {
         let (ct, len) = wasi::args_sizes_get().unwrap();
-        let mut env_positions: Vec<i32> = vec![0; ct];
-        let mut env_data: Vec<u8> = vec![0; len];
+        let mut cmd_arg_positions: Vec<i32> = vec![0; ct];
+        let mut cmd_arg_data: Vec<u8> = vec![0; len];
         wasi::args_get(
-            env_positions.as_mut_ptr() as *mut *mut u8,
-            env_data.as_mut_ptr() as *mut u8,
+            cmd_arg_positions.as_mut_ptr() as *mut *mut u8,
+            cmd_arg_data.as_mut_ptr() as *mut u8,
         )
         .unwrap();
         for x in 0..ct {
-            let base = env_data.as_mut_ptr() as usize;
-            let beg = env_positions[x] as usize - base;
+            let base = cmd_arg_data.as_mut_ptr() as usize;
+            let beg = cmd_arg_positions[x] as usize - base;
             let end = if x == ct - 1 {
-                env_data.len() - 1
+                cmd_arg_data.len() - 1
             } else {
-                env_positions[x + 1] as usize - 1 - base
+                cmd_arg_positions[x + 1] as usize - 1 - base
             };
-            let cmd_arg = String::from_utf8_lossy(&env_data[beg..end]);
+            let cmd_arg = String::from_utf8_lossy(&cmd_arg_data[beg..end]);
             cmd_args.push(cmd_arg.to_string());
         }
     }
@@ -80,6 +80,18 @@ pub fn environment_variables() -> Vec<EnvironmentalVariable> {
         }
     }
     envs
+}
+
+pub fn random() -> f32 {
+    unsafe {
+        let mut noise: Vec<u8> = vec![0; 8];
+        let max = 0xFFFFFFFFFFFFFFFF as u64;
+        let mut array = [0 as u8; 8];
+        wasi::random_get(noise.as_mut_ptr() as *mut u8, 8);
+        array.copy_from_slice(&noise); 
+        let value = u64::from_be_bytes(array);
+        value as f32/max  as f32
+    }
 }
 
 pub fn read_text(path: &str) -> String {
